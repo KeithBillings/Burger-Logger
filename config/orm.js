a30c70dc2954dda426a7fa6a8438c9accde53047
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-08-03 11:44:34 -0700

    add connection and ORM

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+const connection = require('./connection');
+
+const orm = {
+  selectAll: function (callback) {
+    connection.query(`SELECT * FROM burgers_db.burgers`,
+      function (err, res) {
+        if (err) throw err;
+        callback(res);
+      })
+  },
+  insertOne: function (burger_name, devoured, callback) {
+    connection.query(
+      `INSERT INTO burgers (burger_name, devoured) 
+      VALUES ('${burger_name}', ${devoured});`,
+      function (err, res) {
+        if (err) throw err;
+        callback(res);
+      }
+    )
+  },
+  updateOne: function (devoured, id, callback) {
+    connection.query(
+      `UPDATE burgers_db.burgers SET devoured = "${devoured}" WHERE id = "${id}"`,
+      function (err, res) {
+        if (err) throw err;
+        callback(res);
+      }
+    )
+  }
+}
+
+module.exports = orm;
